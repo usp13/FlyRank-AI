25076566e809f954d7b63337dc476b0496ffba06
Utsav Panchal <utsavpanchal1304@gmail.com>
2026-07-28 19:35:35 +0530

    Stage 1: root and health endpoints

--- server.js
+++ server.js
@@ -4,8 +4,21 @@ const app = express();
 
 const PORT = 3000;
 
+// Middleware to parse JSON
+app.use(express.json());
+
+// GET /  - home or base  
 app.get('/', (req, res) => {
-  res.send('Hello from Task API stage 0 !');
+  res.json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"]
+  });
+});
+
+// GET /health -  Health check
+app.get('/health', (req, res) => {
+  res.json({ status: "ok" });
 });
 
 app.listen(PORT, () => {
